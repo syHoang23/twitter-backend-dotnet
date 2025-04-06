01002e76fbf2e842e8981e51b71956da4f412839
syHoang23 <equalforever123@gmail.com>
2025-04-06 17:40:12 +0700

    create file, add scope Interface, repository, create method addservices

---
+++ twitter-dotnet/ServicesRegistration.cs
@@ -0,0 +1,17 @@
+
+
+using DotnetAPI.Repository;
+using DotnetAPI.Repository.Interfaces;
+
+namespace DotnetAPI
+{
+    public static class ServicesRegistration
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IPostRepo, PostRepo>();
+            services.AddScoped<IUserRepo, UserRepo>();
+            services.AddScoped<IAuthRepo, AuthRepo>();
+        }
+    }
+}
