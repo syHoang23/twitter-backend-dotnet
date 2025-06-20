23273befe34f80dfca9299baf1fb17effc8169d7
syHoang23 <equalforever123@gmail.com>
2025-06-20 23:07:15 +0700

    create new UpdateUser

--- twitter-dotnet/Repository/IUserRepo.cs
+++ twitter-dotnet/Repository/IUserRepo.cs
@@ -1,3 +1,4 @@
+using DotnetAPI.Dtos;
 using DotnetAPI.Models;
 namespace DotnetAPI.Repository.Interfaces
 {
@@ -6,6 +7,7 @@ namespace DotnetAPI.Repository.Interfaces
         IEnumerable<User> GetUsers(int userId = 0, bool isActive = false);
         IEnumerable<User> GetUserProfile(int userId);
         bool UpsertUser(User user);
+        bool UpdatetUser(int userId, UserForUpdateDto userForUpdate);
         bool DeleteUser(int userId);
     }
 }
