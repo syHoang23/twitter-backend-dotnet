c50e500fe6bafea7c1bcacd772d255725a268363
syHoang23 <equalforever123@gmail.com>
2025-06-20 23:07:00 +0700

    rename ResetPassword to CreatePassword and create new ResetPassword

--- twitter-dotnet/Repository/IAuthRepo.cs
+++ twitter-dotnet/Repository/IAuthRepo.cs
@@ -5,7 +5,8 @@ namespace DotnetAPI.Repository.Interfaces
     public interface IAuthRepo
     {
         bool Register(UserForRegistrationDto userForRegistration);
-        bool ResetPassword(UserForLoginDto userForSetPassword);
+        bool CreatePassword(UserForLoginDto userForSetPassword);
+        bool ResetPassword(int userId, string password);
         Dictionary<string, string> Login(UserForLoginDto userForLogin);
         string RefreshToken(int userIdParam);
     }
