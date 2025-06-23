7f08190fe71f18774e26aef239d6ca57f961ec00
syHoang23 <equalforever123@gmail.com>
2025-06-23 19:52:19 +0700

    create new Follow Controller

---
+++ twitter-dotnet/Controllers/FollowController.cs
@@ -0,0 +1,14 @@
+using DotnetAPI.Dtos;
+using DotnetAPI.Repository;
+using DotnetAPI.Repository.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers
+{
+    [Authorize]
+    public class FollowController : BaseApiController
+    {
+        
+    }
+}
